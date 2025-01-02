bca882e54a73344c5a32be60854bc55bc275c087
1GodLess5 <gacekalbert@seznam.cz>
2025-01-02 14:29:18 +0100

    Initial commit

---
+++ src/main/java/cz/gacalbert/AD_shifter/AdShifterApplication.java
@@ -0,0 +1,13 @@
+package cz.gacalbert.AD_shifter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdShifterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdShifterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cz/gacalbert/AD_shifter/AdShifterApplicationTests.java
@@ -0,0 +1,13 @@
+package cz.gacalbert.AD_shifter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdShifterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
